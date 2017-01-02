4f1ddf1f2a40800575bb1131cd9a0e35def091d9
Igor Paprzycki <igypap@gmail.com>
2017-01-02 22:08:56 +0100

    Final commit

--- src/com/igypap/challange/League.java
+++ src/com/igypap/challange/League.java
@@ -1,8 +1,10 @@
 package com.igypap.challange;
 
+import com.igypap.Player;
 import com.igypap.Team;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Created by igypap on 02.01.17.
@@ -11,6 +13,23 @@ public class League<T extends Team> {
     private String name;
     private ArrayList<T> league = new ArrayList<T>();
 
+    public League(String name) {
+        this.name = name;
+    }
 
+    public boolean add(T team) {
+        if (league.contains((team))) {
+            return false;
+        }
+        league.add(team);
+        return true;
+    }
+
+    public void showLeagueTable() {
+        Collections.sort(league);
+        for (T t : league) {
+            System.out.println(t.getName() + ": " + t.ranking());
+        }
+    }
 }
 
--- src/com/igypap/challange/Main.java
+++ src/com/igypap/challange/Main.java
@@ -1,5 +1,10 @@
 package com.igypap.challange;
 
+import com.igypap.BaseballPlayer;
+import com.igypap.FootballPlayer;
+import com.igypap.SoccerPlayer;
+import com.igypap.Team;
+
 /**
  * Created by igypap on 02.01.17.
  */
@@ -15,6 +20,62 @@ public class Main {
         // Only teams of the same type should be added to any particular
         // instance of the league class - the program should fail to compile
         // if an attempt is made to add an incompatible team.
+
+
+        League<Team<FootballPlayer>> footballLeague = new League<>("AFL");
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        Team<FootballPlayer> hawthorn = new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+
+
+        hawthorn.matchResult(fremantle, 1, 0);
+        hawthorn.matchResult(adelaideCrows, 3, 8);
+        adelaideCrows.matchResult(fremantle, 2, 1);
+
+        footballLeague.add(adelaideCrows);
+        footballLeague.add(melbourne);
+        footballLeague.add(hawthorn);
+        footballLeague.add(fremantle);
+
+//        footballLeague.add(baseballTeam); // not working - good
+        footballLeague.showLeagueTable();
+
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        Team rawTeam = new Team("Raw Team");
+        rawTeam.addPlayer(beckham);         // unchecked warning
+        rawTeam.addPlayer(pat);             // unchecked warning
+
+        footballLeague.add(rawTeam);        // unchecked warning
+
+        League<Team> rawLeague = new League<>("Raw");
+        rawLeague.add(adelaideCrows);       //no warning
+        rawLeague.add(baseballTeam);        //no warning
+        rawLeague.add(rawTeam);             //no warning
+
+        League reallyRaw = new League("Really Raw");
+        reallyRaw.add(adelaideCrows);       // unchecked warning
+        reallyRaw.add(baseballTeam);        // unchecked warning
+        reallyRaw.add(rawTeam);             // unchecked warning
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
